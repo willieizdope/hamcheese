07635b39d9184b1be1e3fddeef3656ce4ee4eee3
Willie Vogel <wivo1351@colorado.edu>
2021-04-22 16:54:26 -0600

    first commit of working html 5 boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+ },
+ module: {
+   rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. injects css into DOM
+          "css-loader", // 2.turns css into commonjs
+          "sass-loader", // 1.turns sass into css
+        ],
+      },
+    ],
+  },
+};
